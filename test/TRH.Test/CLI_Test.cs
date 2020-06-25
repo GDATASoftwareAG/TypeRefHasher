2d1a5d85111242c2d50b70bd22c921b56e6af61f
secana <stefan.hausotte@gmx.de>
2020-06-25 14:01:50 +0200

    debug

--- test/TRH.Test/CLI_Test.cs
+++ test/TRH.Test/CLI_Test.cs
@@ -37,8 +37,8 @@ namespace TRH.Test
 
         private static string GetCliTool() 
             => Environment.OSVersion.Platform == PlatformID.Unix
-                ? Path.GetFullPath("../../../../artifacts/trh")
-                : Path.GetFullPath("../../../../artifacts/trh.exe");
+                ? Path.GetFullPath("../../../../../artifacts/trh")
+                : Path.GetFullPath("../../../../../artifacts/trh.exe");
 
 
         [Fact]
