8f4fba36db6666c7689e389c09c826fc2ac14f7b
Stefan Hausotte <stefan.hausotte@gdata.de>
2020-06-25 12:06:41 +0200

    Add program logic

--- src/TRH/Program.cs
+++ src/TRH/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using PeNet;
+using File = System.IO.File;
 
 namespace TRH
 {
     class Program
     {
-        static void Main(string[] args)
+        private const int Error = 0;
+        private const int Success = 1;
+
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: trh pathtofile");
+                return Error;
+            }
+
+            var file = args[0];
+            if (!File.Exists(file))
+            {
+                Console.Error.WriteLine("File not found.");
+                return Error;
+            }
+
+            if (!PeFile.TryParse(file, out var peFile))
+            {
+                Console.Error.WriteLine("Not a valid PE file.");
+                return Error;
+            }
+
+            if (!peFile!.IsDotNet)
+            {
+                Console.Error.WriteLine("Not a valid .NET binary.");
+                return Error;
+            }
+
+            try
+            {
+                Console.WriteLine(peFile.TypeRefHash);
+            }
+            catch (Exception) 
+            {
+                Console.Error.WriteLine("Cannot compute TypeRefHash.");
+                return Error;
+            }
+
+            return Success;
         }
     }
 }
